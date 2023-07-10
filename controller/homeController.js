debf188e7920d299c5a32233946737998a026062
Yashwant Sonkar <yashsonkar0102@gmail.com>
2023-07-10 15:21:47 +0530

    Setup Ejs layouts

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,5 +1,12 @@
 
 
 module.exports.home = (req, res) => {
-    return res.send("<h1>This is Home Controller</h1>");
+    // return res.send("<h1>This is Home Controller</h1>");
+    try {
+        return res.render('Home' , {
+            title: 'Home',
+        });
+    } catch (error) {
+        console.log('Error in homeController ', error);
+    }
 };
--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 // setting up express server
-const express = require('express');
-const app = express();
+const express           = require('express');
+const app               = express();
+const expressLayout     = require('express-ejs-layouts');
 
 // defining port number
-const port = process.env.port || 8000;
+const port              = process.env.port || 8000;
+
+
+// setting up view engine
+app.set('view engine', 'ejs');
+app.set('views','./views');
+
+
+// Layouts
+app.use(expressLayout);
+
+// extract styles and script from sub pages into layouts
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 
 // Acquiring all the routes

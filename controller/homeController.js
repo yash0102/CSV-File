f85872d695dfbe6b2a40ff5a5d418de186ff4817
Yashwant Sonkar <yashsonkar0102@gmail.com>
2023-07-11 23:00:39 +0530

    Uploaded file functionality

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,9 +1,9 @@
-const file = require('../model/csv')
+const csvFile = require('../model/csv')
 
 module.exports.home = async (req, res) => {
     // return res.send("<h1>This is Home Controller</h1>");
     try {
-        let files = await file.find({});
+        let files = await csvFile.find({});
 
         return res.render('Home' , {
             title: 'Home',
@@ -13,3 +13,34 @@ module.exports.home = async (req, res) => {
         console.log('Error in homeController ', error);
     }
 };
+
+
+// upload a file
+module.exports.upload = async (req, res) => {
+    try {
+
+        // console.log("req file", req.file);
+
+        if(!req.file) {
+            return res.status(400).send('Please Upload a file!');
+        }
+
+        if(!req.file.mimetype.startsWith('text/csv')) {
+            return res.status(400).send('Please selcet CSV files only!');
+        }
+        
+    } catch (error) {
+        console.log('Error in uploading files', error);
+    }
+
+    const { originalname , path , filename } = req.file;
+
+    const file = await csvFile.create({
+        FileName: originalname,
+        FilePath: path,
+        File: filename
+    });
+
+
+    return res.redirect('/');
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controller/homeController');
+const express               = require('express');
+const router                = express.Router();
+const multer                = require('multer');
+const controller            = require('../controller/homeController');
+
+
+const upload = multer({ dest:'uploadFiles' })
 
 
 router.get('/', controller.home);
+
+router.post('/upload',upload.single('file'), controller.upload);
+
 
 
 
